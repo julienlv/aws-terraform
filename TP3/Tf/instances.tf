6c031d6d1aad54e70787980b8edf3e2ab0c91bab
Julien Le Van <j.levan@ecole-ipssi.net>
2020-08-21 10:21:33 +0200

    Update instances.tf

--- TP3/Tf/instances.tf
+++ TP3/Tf/instances.tf
@@ -10,20 +10,20 @@ resource "aws_key_pair" "tfkeypair1" {
 # Adapter le nom à l'usage
 resource "aws_security_group" "sg_wordpress" {
   name   = "sg_wordpress"
-  vpc_id = aws_vpc.vps_wordpress.id
+  vpc_id = aws_vpc.vpc_example.id
   # en entrée
-  # autorise ssh de partout
+  # autorise la communication avec MariaDB
   ingress {
-    from_port   = "22"
-    to_port     = "22"
+    from_port   = "0"
+    to_port     = "0"
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.42.1.20/32"]
   }
   # autorise http de partout
   ingress {
     from_port   = "80"
     to_port     = "80"
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   # autorise icmp (ping)
@@ -35,10 +35,18 @@ resource "aws_security_group" "sg_wordpress" {
   }
 }
 
-resource "aws_security_group" "sg_wordpress_mariadb" {
-  name   = "sg_wordpress_mariadb"
-  vpc_id = aws_vpc.vps_wordpress_mariadb.id
+# Adapter le nom à l'usage
+resource "aws_security_group" "sg_mariadb" {
+  name   = "sg_mariadb"
+  vpc_id = aws_vpc.vpc_example.id
   # en entrée
+  # autorise Wordpress
+  ingress {
+    from_port   = "3306"
+    to_port     = "3306"
+    protocol    = "tcp"
+    cidr_blocks = ["10.42.1.10/32"]
+  }
   # autorise ssh de partout
   ingress {
     from_port   = "22"
@@ -46,56 +54,42 @@ resource "aws_security_group" "sg_wordpress_mariadb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # autorise http de partout
-  ingress {
-    from_port   = "3306"
-    to_port     = "3306"
-    protocol    = "tcp"
-    cidr_blocks = ["10.42.1.0/24"]
-  }
-  # autorise icmp (ping)
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-resource "aws_instance" "wordpress" {
+resource "aws_instance" "Wordpress" {
   # Ubuntu 18.04 fournie par AWS
   ami                         = "ami-0bcc094591f354be2"
   instance_type               = "t2.micro"
   key_name                    = "tfkeypair1"
   vpc_security_group_ids      = [aws_security_group.sg_wordpress.id]
-  subnet_id                   = aws_subnet.subnet_wordpress.id
+  subnet_id                   = aws_subnet.subnet_example.id
   private_ip                  = "10.42.1.10"
   associate_public_ip_address = "true"
   user_data                   = file("../Scripts/instance_init1.sh")
   tags = {
-    Name = "wordpress"
+    Name = "Wordpress"
   }
 }
 
-resource "aws_instance" "wordpress_mariadb" {
+resource "aws_instance" "MariaDB" {
   # Ubuntu 18.04 fournie par AWS
   ami                         = "ami-0bcc094591f354be2"
   instance_type               = "t2.micro"
   key_name                    = "tfkeypair1"
-  vpc_security_group_ids      = [aws_vpc.vps_wordpress_mariadb.id]
-  subnet_id                   = aws_subnet.subnet_wordpress.id
-  private_ip                  = "10.42.1.100"
+  vpc_security_group_ids      = [aws_security_group.sg_mariadb.id]
+  subnet_id                   = aws_subnet.subnet_example.id
+  private_ip                  = "10.42.1.20"
   associate_public_ip_address = "true"
   user_data                   = file("../Scripts/instance_init2.sh")
   tags = {
-    Name = "wordpress_mariadb"
+    Name = "MariaDB"
   }
 }
 
-output "wordpress" {
-  value = "${aws_instance.wordpress.*.public_ip}"
+output "Wordpress_ip" {
+  value = "${aws_instance.Wordpress.*.public_ip}"
 }
 
-output "wordpress_mariadb" {
-  value = "${aws_instance.wordpress_mariadb.*.public_ip}"
+output "MariaDB_ip" {
+  value = "${aws_instance.MariaDB.*.public_ip}"
 }
